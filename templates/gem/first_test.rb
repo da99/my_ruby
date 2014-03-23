829d0e0030b1fa968b6d7191802f34bcea3a4b2b
da99 <i-hate-spam-1234567@mailinator.com>
2014-03-23 17:48:11 -0500

    Fixed: Bacon_Colored now avoiding -r

--- templates/gem/first_test.rb
+++ templates/gem/first_test.rb
@@ -1,11 +1,8 @@
 
-require "{{name}}"
-
-
 describe "{{name}}" do
 
   it "runs" do
-    
+    {{NAME}}.should.flunk "No tests written."
   end
 
 end # === describe {{name}} ===
--- templates/gem/helpers.rb
+++ templates/gem/helpers.rb
@@ -1,2 +1,3 @@
 
 require 'Bacon_Colored'
+require '{{name}}'
