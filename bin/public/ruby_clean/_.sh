13d1b4ec6b843009be4d6f78abf97d3ce445e376
da99 <i-hate-spam-1234567@mailinator.com>
2017-05-10 03:15:46 -0500

    Changed: bash_setup -> my_bash

--- bin/public/ruby_clean/_.sh
+++ bin/public/ruby_clean/_.sh
@@ -1,8 +1,8 @@
 
 # === {{CMD}}   # Checks if .rb files contain aputs, puts, binding.pry
 ruby_clean () {
-    PATH="$PATH:$THIS_DIR/../bash_setup/bin"
-    bash_setup files_zero aputs '*.rb'
-    bash_setup files_zero puts  '*.rb'
-    bash_setup files_zero binding.pry  '*.rb'
+    PATH="$PATH:$THIS_DIR/../my_bash/bin"
+    files_zero aputs '*.rb'
+    files_zero puts  '*.rb'
+    files_zero binding.pry  '*.rb'
 } # === end function
